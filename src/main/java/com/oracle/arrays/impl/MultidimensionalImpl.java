8dcf8bc8137b369f6e6b387d01a10c0ff5f86414
Oswaldo Franco <oswaldo.franco@lagarto.com.mx>
2022-09-07 17:09:36 -0500

    Add: implementacion de metodos para manejo de arrays

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -6,11 +6,19 @@ import com.oracle.arrays.model.Posicion;
 public class MultidimensionalImpl implements Multidimensional {
     @Override
     public Posicion getPosition(String[][] datos, String dato) {
+        for (int i=0; i<datos.length;i++){
+            for (int j=0; j<datos[i].length; j++){
+                if (dato.equals(datos[i][j])){
+
+                    return new Posicion(i,j);
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        return null;
+        return datos[posicion.getX()][posicion.getY()];
     }
 }
--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,10 +5,28 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        for (int i = 0; i <arr.length; i++) {
+            for (int j = i+1; j <arr.length; j++) {
+                if(arr[i] > arr[j]) {      //swap elements if not in order
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        for (int i = 0; i <arr.length; i++) {
+            for (int j = i+1; j <arr.length; j++) {
+                if(arr[i] < arr[j]) {      //swap elements if not in order
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
