ae1dffc2e8a9dfe3b0e9ba0163a6305c0b4fa20f
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2022-09-07 17:09:54 -0500

    Solve arrays exercises

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -6,11 +6,20 @@ import com.oracle.arrays.model.Posicion;
 public class MultidimensionalImpl implements Multidimensional {
     @Override
     public Posicion getPosition(String[][] datos, String dato) {
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                if (datos[i][j].equals(dato)) return new Posicion(i, j);
+            }
+        }
+
         return null;
     }
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        return null;
+        int x = posicion.getX();
+        int y = posicion.getY();
+
+        return datos[x][y];
     }
 }
--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -2,13 +2,31 @@ package com.oracle.arrays.impl;
 
 import com.oracle.arrays.Unidimensional;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        Arrays.sort(arr);
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        int[] arrRev = arr.clone();
+
+        Arrays.sort(arrRev);
+
+        int arrLength = arr.length;
+
+        for (int i = 0; i < arrLength / 2; i++) {
+            int temp = arrRev[i];
+            int j = arrLength - i - 1;
+
+            arrRev[i] = arrRev[j];
+            arrRev[j] = temp;
+        }
+
+        return arrRev;
     }
 }
