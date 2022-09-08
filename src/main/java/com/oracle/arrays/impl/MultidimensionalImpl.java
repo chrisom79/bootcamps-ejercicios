1750ca171150088ca6e4615480a90e91551c189c
Hugo Castillo <hugocastillopelaez@gmail.com>
2022-09-07 19:15:18 -0500

    Implemented methods to pass unit tests.

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -5,12 +5,22 @@ import com.oracle.arrays.model.Posicion;
 
 public class MultidimensionalImpl implements Multidimensional {
     @Override
-    public Posicion getPosition(String[][] datos, String dato) {
+    public Posicion getPosition(String[][] datos, String dato)
+    {
+        for(int i = 0; i < datos.length; i++){
+            for(int j = 0; j < datos[0].length; j++){
+                if(datos[i][j].equals(dato)){
+                    return new Posicion(i,j);
+                }
+            }
+        }
         return null;
     }
 
     @Override
-    public String getData(String[][] datos, Posicion posicion) {
-        return null;
+    public String getData(String[][] datos, Posicion posicion)
+    {
+        return datos[posicion.getX()][posicion.getY()];
+
     }
 }
