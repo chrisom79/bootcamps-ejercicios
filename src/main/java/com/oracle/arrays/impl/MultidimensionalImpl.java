6a8dfee95bf6da1a152e37945c20ba0d32f25d76
Gustavo Adolfo Carrillo Camacho <gustavoadolfocarrillocamacho@gmail.com>
2022-09-10 17:06:36 -0500

    Update MultidimensionalImpl.java

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -4,13 +4,28 @@ import com.oracle.arrays.Multidimensional;
 import com.oracle.arrays.model.Posicion;
 
 public class MultidimensionalImpl implements Multidimensional {
+    Posicion position;
+
     @Override
     public Posicion getPosition(String[][] datos, String dato) {
+        position = new Posicion(0, 0);
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                if (datos[i][j].equals(dato)) {
+                    position.setX(i);
+                    position.setY(j);
+                    return position;
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        return null;
+        String data;
+        data = datos[posicion.getX()][posicion.getY()];
+        return data;
     }
+
 }
