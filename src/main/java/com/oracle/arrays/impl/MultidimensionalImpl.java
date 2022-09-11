5d3c639f9aed42db5bb58729919fc0ee44f87d3e
ed9ar <a01339939@itesm.mx>
2022-09-10 20:04:06 -0500

    Update MultidimensionalImpl.java

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -8,18 +8,17 @@ public class MultidimensionalImpl implements Multidimensional {
     public Posicion getPosition(String[][] datos, String dato) {
         Posicion pos = new Posicion(0, 0);
         for(int i = 0; i < datos.length; i++){
-            for(int j = 0; j < datos[0].length;j++){
+            for(int j = 0; j < datos[i].length;j++){
                 if(datos[i][j].equals(dato)){
                     pos.setX(i);
                     pos.setY(j);
+                    return pos;
                 }
             }
         }
-        if(datos.length == 0){
-            return null;
-        }else{
-            return pos;
-        }
+        
+        return null;
+        
         
     }
 
