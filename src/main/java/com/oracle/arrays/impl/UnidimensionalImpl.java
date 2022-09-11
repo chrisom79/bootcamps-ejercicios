5b831692d10339f2383620c749ea745501fa4c51
Brandon Herrera <brandondh769@gmail.com>
2022-09-11 17:57:45 -0500

    Implementación terminada

--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,10 +5,28 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        for (int j = 0; j < arr.length; j++){
+            for (int i = 0; i < arr.length - 1; i++){
+                if( arr[i] > arr[i + 1]) {
+                    int aux = arr[i+1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = aux;
+                }
+            }
+        }
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        for (int j = 0; j < arr.length; j++){
+            for (int i = 0; i < arr.length - 1; i++){
+                if( arr[i] < arr[i + 1]) {
+                    int aux = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = aux;
+                }
+            }
+        }
+        return arr;
     }
 }
