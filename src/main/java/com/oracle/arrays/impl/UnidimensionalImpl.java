a2e556e483ac975e25ab313ca1d099f1f0d9a8a0
rValeM <87331123+rValeM@users.noreply.github.com>
2022-09-10 22:40:52 -0500

    Update UnidimensionalImpl.java

--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,10 +5,36 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        boolean flag=false;
+        while(!flag){
+            flag=true;
+            for(int i=0;i<arr.length-1;i++){
+                if(arr[i]>arr[i+1]){
+                    int a=arr[i];
+                    int b=arr[i+1];
+                    arr[i+1]=a;
+                    arr[i]=b;
+                    flag=false;
+                }
+            }
+        }
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        boolean flag=false;
+        while(!flag){
+            flag=true;
+            for(int i=0;i<arr.length-1;i++){
+                if(arr[i]<arr[i+1]){
+                    int a=arr[i];
+                    int b=arr[i+1];
+                    arr[i+1]=a;
+                    arr[i]=b;
+                    flag=false;
+                }
+            }
+        }
+        return arr;
     }
 }
