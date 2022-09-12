e60aefa9abc6411be4348e590790a174d9e31f19
Itzman Montiel <itz_mont09@outlook.com>
2022-09-11 20:14:52 -0500

    Ejercicio Resuelto

--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,10 +5,39 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        //Existen diferentes algoritmos de ordanmiento pero para este ejercicio realice el mas sencillo
+
+        int val;
+        //System.out.println(arr.length);
+        for (int i = 0;i < arr.length-2;i++){
+            if (arr[i]>arr[i+1]){
+                val = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = val;
+            }
+        }
+        /*
+        for(int i = 0; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        */
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        int[] reverse = new int[4];
+        int j = arr.length - 1;
+        //System.out.println("Empieza");
+        for(int i = 0; i < arr.length ;i++){
+            reverse[i] = arr[j];
+            //System.out.println(reverse[i]);
+            j--;
+        }
+        /*
+        for(int i = 0; i< arr.length;i++){
+            System.out.println(reverse[i]);
+        }
+        */
+        return reverse;
     }
 }
