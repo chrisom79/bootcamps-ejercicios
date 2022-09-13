cd4db70f1d28de754da2b78ab2ae10f236b04a07
Adolfo Carrillo <gustavoadolfocarrillocamacho@gmail.com>
2022-09-13 14:47:25 -0500

    Changes applied in multidimentional array

--- bootcamps-ejercicios/src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ bootcamps-ejercicios/src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -4,17 +4,14 @@ import com.oracle.arrays.Multidimensional;
 import com.oracle.arrays.model.Posicion;
 
 public class MultidimensionalImpl implements Multidimensional {
-   Posicion position;
 
     @Override
     public Posicion getPosition(String[][] datos, String dato) {
-        position = new Posicion(0, 0);
+        
         for (int i = 0; i < datos.length; i++) {
             for (int j = 0; j < datos[i].length; j++) {
                 if (datos[i][j].equals(dato)) {
-                    position.setX(i);
-                    position.setY(j);
-                    return position;
+                    return new Posicion(i, j);
                 }
             }
         }
@@ -23,9 +20,7 @@ public class MultidimensionalImpl implements Multidimensional {
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        String data;
-        data = datos[posicion.getX()][posicion.getY()];
-        return data;
+        
+        return datos[posicion.getX()][posicion.getY()];
     }
-
 }
