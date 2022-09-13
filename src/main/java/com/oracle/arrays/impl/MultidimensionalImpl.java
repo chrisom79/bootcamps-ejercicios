2ac1fd6210012ebdbd008c8c2826092fc74d50a9
carlosveg <carlosvegapro@gmail.com>
2022-09-12 20:58:25 -0500

    Terminando ejercicios

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -3,14 +3,25 @@ package com.oracle.arrays.impl;
 import com.oracle.arrays.Multidimensional;
 import com.oracle.arrays.model.Posicion;
 
+import java.util.Arrays;
+
 public class MultidimensionalImpl implements Multidimensional {
     @Override
     public Posicion getPosition(String[][] datos, String dato) {
-        return null;
+        Posicion pos = null;
+
+        for (int i = 0; i < datos.length; i++)
+            for (int j = 0; j < datos[i].length; j++)
+                if (datos[i][j].equals(dato)) {
+                    pos = new Posicion(i, j);
+                    break;
+                }
+
+        return pos;
     }
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        return null;
+        return datos[posicion.getX()][posicion.getY()];
     }
 }
--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -2,13 +2,20 @@ package com.oracle.arrays.impl;
 
 import com.oracle.arrays.Unidimensional;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        Arrays.sort(arr);
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        return Arrays.stream(arr).boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
