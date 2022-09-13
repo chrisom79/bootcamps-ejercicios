0dfbe3d0a13b95a4279f2b473b08bcf38a99e91a
Gustavo Adolfo Carrillo Camacho <gustavoadolfocarrillocamacho@gmail.com>
2022-09-13 13:29:03 -0500

    Unidimentional cahnges sugested by Christian Gomez
    I'd used the divide and conquer technique by implementing Quiksort on the Unidimentional array.

--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,32 +5,59 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i+1; j< arr.length; j++){
-                if(arr[i]> arr[j]){
-                    int temp = arr[i];
-                    arr[i]= arr[j];
-                    arr[j]= temp;
-                }
-            }
-        }
+
+        quickSort(arr, 0, arr.length - 1, true);
+
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-         for(int i = 0; i < arr.length; i++){
-            for(int j = i+1; j< arr.length; j++){
-                if(arr[i]< arr[j]){
-                    int temp = arr[i];
-                    arr[i]= arr[j];
-                    arr[j]= temp;
+
+        int cpyArr[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            cpyArr[i] = arr[i];
+        }
+
+        quickSort(cpyArr, 0, cpyArr.length - 1, false);
+        return cpyArr;
+
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high, boolean direction)//true ascendent, false descendent
+    {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high - 1; j++) {
+            if (direction == true) {
+                if (arr[j] < pivot) {
+                    i++;
+                    swap(arr, i, j);
+                }
+            } else if (direction == false) {
+                if (arr[j] > pivot) {
+                    i++;
+                    swap(arr, i, j);
                 }
             }
         }
-         int cpyArr [] = new int[arr.length];
-         for(int i = 0; i<arr.length; i++){
-             cpyArr[i]= arr[i];
-         }
-        return cpyArr;
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
+
+    static void quickSort(int[] arr, int low, int high, boolean direction) {
+        if (low < high) {
+
+            int pi = partition(arr, low, high, direction);
+
+            quickSort(arr, low, pi - 1, direction);
+            quickSort(arr, pi + 1, high, direction);
+        }
     }
 }
