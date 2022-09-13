f30b3789ee832a38dd7945b05bdf20219eb49cad
victorCarr <victor11298@gmail.com>
2022-09-13 16:10:03 -0500

    Realización de ejercicios

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -6,11 +6,24 @@ import com.oracle.arrays.model.Posicion;
 public class MultidimensionalImpl implements Multidimensional {
     @Override
     public Posicion getPosition(String[][] datos, String dato) {
-        return null;
+        // Coding
+        Posicion p = null;
+        for(int i=0; i<datos.length; i++){
+            for(int j=0; j<datos[i].length; j++){
+                if(datos[i][j].equals(dato)){
+                    p = new Posicion(i,j);
+                    return p;
+                }
+            }
+        }
+        return p;
     }
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        return null;
+        //Coding
+        int x = posicion.getX();
+        int y = posicion.getY();
+        return datos[x][y];
     }
 }
--- src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/UnidimensionalImpl.java
@@ -5,10 +5,30 @@ import com.oracle.arrays.Unidimensional;
 public class UnidimensionalImpl implements Unidimensional {
     @Override
     public void ordenamiento(int[] arr) {
+        // Coding
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public int[] ordenamientoReversa(int[] arr) {
-        return null;
+        // Coding
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-1; j++){
+                if(arr[j]<arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
     }
 }
