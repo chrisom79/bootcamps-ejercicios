dd0427810f92bc62d6e3201285e9bde6afe8c6d8
Gustavo Adolfo Carrillo Camacho <gustavoadolfocarrillocamacho@gmail.com>
2022-09-13 13:58:20 -0500

    Changes suggested by Christian Gomez Applied
    On the getPosition method the return statement was simplified just returning the constructor initialized with the indexes where the seeked data is, instead of initializing the setters explicitly.
    
    On the getData method the declaration of variables was reduced at it's minimum just returning the data on the given position instead of declaring a string variable and assigning it the value at the bidimentional array "datos" that the given position holds.
    
    Great assertions, Thanks for sharing your knowledge Christian!!!

--- src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
+++ src/main/java/com/oracle/arrays/impl/MultidimensionalImpl.java
@@ -6,15 +6,13 @@ import com.oracle.arrays.model.Posicion;
 public class MultidimensionalImpl implements Multidimensional {
     Posicion position;
 
-    @Override
+     @Override
     public Posicion getPosition(String[][] datos, String dato) {
-        position = new Posicion(0, 0);
+        
         for (int i = 0; i < datos.length; i++) {
             for (int j = 0; j < datos[i].length; j++) {
                 if (datos[i][j].equals(dato)) {
-                    position.setX(i);
-                    position.setY(j);
-                    return position;
+                    return new Posicion(i, j);
                 }
             }
         }
@@ -23,9 +21,8 @@ public class MultidimensionalImpl implements Multidimensional {
 
     @Override
     public String getData(String[][] datos, Posicion posicion) {
-        String data;
-        data = datos[posicion.getX()][posicion.getY()];
-        return data;
+        
+        return datos[posicion.getX()][posicion.getY()];
     }
 
 }
