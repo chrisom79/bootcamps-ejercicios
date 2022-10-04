a752cba570dddeb10dd8198f7dc795d47ab2b0e9
agranados <agranados@itnetworks.mx>
2022-10-03 20:05:08 -0500

    Unit Test succes from Dynamic Array List

--- src/main/java/com/oracle/arrays/impl/DynamicArrayListImpl.java
+++ src/main/java/com/oracle/arrays/impl/DynamicArrayListImpl.java
@@ -4,8 +4,8 @@ import com.oracle.arrays.DynamicArrayList;
 
 public class DynamicArrayListImpl implements DynamicArrayList {
     private int size = 10;
-    private int[] arr = new int[size];
     private int idx = 0;
+    private int[] arr = new int[size];
 
     public Integer getSize() {
         return size;
@@ -17,35 +17,49 @@ public class DynamicArrayListImpl implements DynamicArrayList {
 
     @Override
     public void insert(int element) {
-        if(idx >= size) {
-            resize();
+        if (idx == (int) (size * .70)) {
+            resize();            
         }
-
         arr[idx] = element;
+        idx++;
     }
 
     @Override
-    public void remove(int index) {
-
+    public void remove(int index) {        
+        arr[idx + 1] = 0;
+        for (int i = index+1; i < idx; i++) {
+            arr[i] = arr[i+1];
+        }
+        idx--;
     }
 
     @Override
     public int get(int index) {
-        return 0;
+        return arr[index];
     }
 
     @Override
     public void resize() {
-
+        size = (int) (size * .40) + size;
+        int temp[] = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+        arr = temp;       
     }
 
     @Override
     public void resize(int newSize) {
-
+        size = newSize;
+        int temp[] = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+        arr = temp;
     }
 
     @Override
     public int length() {
-        return 0;
+        return idx;
     }
 }
