e3f1a1fd9e19ccb125ad4458327eb0c302705072
Christian Gomez <omar.christian@gmail.com>
2022-10-05 17:11:58 -0500

    Adding cityDAO

--- src/main/java/com/oracle/stream/CountriesAndCities.java
+++ src/main/java/com/oracle/stream/CountriesAndCities.java
@@ -1,10 +1,12 @@
 package com.oracle.stream;
 
+import com.oracle.dao.CityDao;
 import com.oracle.dao.CountryDao;
 import com.oracle.dao.InMemoryWorldDao;
 
 public class CountriesAndCities {
     private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
+    private static final CityDao cityDao = InMemoryWorldDao.getInstance();
 
     public static void main(String[] args) {
         // Encontrar las ciudades mas pobladas en cada continente
