e1224eaafb37cebe42b3af6c4dbf18cdf28c6b15
Ricardo Valencia <rvalenciamtz@gmail.com>
2022-10-11 10:53:31 -0500

    Ejercicio stream ciudades.

--- src/main/java/com/oracle/stream/CountriesAndCities.java
+++ src/main/java/com/oracle/stream/CountriesAndCities.java
@@ -3,6 +3,11 @@ package com.oracle.stream;
 import com.oracle.dao.CityDao;
 import com.oracle.dao.CountryDao;
 import com.oracle.dao.InMemoryWorldDao;
+import com.oracle.domain.City;
+import com.oracle.domain.Country;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CountriesAndCities {
     private static final CountryDao countryDao = InMemoryWorldDao.getInstance();
@@ -10,5 +15,7 @@ public class CountriesAndCities {
 
     public static void main(String[] args) {
         // Encontrar las ciudades mas pobladas en cada continente
+        Map<String, Optional<City>> ciudadesMasPobladas = cityDao.findAllCities().stream().collect(Collectors.groupingBy(city -> countryDao.findCountryByCode(city.getCountryCode()).getContinent(),Collectors.maxBy(Comparator.comparingInt(City::getPopulation))));
+        ciudadesMasPobladas.entrySet().forEach(System.out::println);
     }
 }
